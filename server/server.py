1361f2341fc2fc618035c8514154f85ff0b1a013
Mohammad <ismail.mohammad.n@gmail.com>
2023-04-09 17:22:13 -0700

    Intiial

--- server/server.py
+++ server/server.py
@@ -1,40 +1,80 @@
-from flask import Flask
+from flask import Flask, request
 
 import networkx as nx
 import matplotlib.pyplot as plt
+import time
 
 app = Flask(__name__)
+
+@app.route("/acceptData", methods=["POST"])
+def get_Input():
+    print(request)
+    startNode = request.form["startNode"]
+    endNode = request.form["endNode"]
+    animate_shortest_path(createGraphTing(), startNode, endNode);
+    return "<p>Acknowledged start and end" + startNode + endNode + " </p>";
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
-# create a graph
-G = nx.Graph()
+def createGraphTing():
+    # create a graph
+    G = nx.Graph()
 
-# add nodes with labels
-nodes = ['A', 'B', 'C', 'D', 'E', 'ISP']
-G.add_nodes_from(nodes)
+    # add nodes
+    G.add_nodes_from(['r1', 'r2', 'r3', 'r4', 'r5', 'r6', 'r7', 'r8', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8'])
 
-# add edges between A,B,C,D,E and ISP
-for node in nodes[:-1]:
-    G.add_edge(node, 'ISP')
+    # add edges
+    G.add_edge('r1', 'r2', weight=1)
+    G.add_edge('r1', 'r3', weight=5)
+    G.add_edge('r2', 'r3', weight=2)
+    G.add_edge('r2', 'r4', weight=3)
+    G.add_edge('r3', 'r5', weight=2)
+    G.add_edge('r4', 'r5', weight=1)
+    G.add_edge('r4', 'r6', weight=4)
+    G.add_edge('r5', 'r7', weight=3)
+    G.add_edge('r6', 'r7', weight=4)
+    G.add_edge('r6', 'r8', weight=2)
+    G.add_edge('r7', 'r8', weight=1)
+    G.add_edge('h1', 'r1', weight=1)
+    G.add_edge('h2', 'r1', weight=1)
+    G.add_edge('h3', 'r1', weight=1)
+    G.add_edge('h4', 'r2', weight=1)
+    G.add_edge('h5', 'r2', weight=1)
+    G.add_edge('h6', 'r3', weight=1)
+    G.add_edge('h7', 'r6', weight=1)
+    G.add_edge('h8', 'r7', weight=1)
+    return G
 
-# Link ISP to TMU
-G.add_edge('ISP', 'TMU')
+def animate_shortest_path(G, source, target):
+    # Find shortest path
+    path = nx.dijkstra_path(G, source=source, target=target, weight='weight')
 
-# position the nodes in a circle
-pos = nx.circular_layout(G)
+    # Highlight nodes and edges on the shortest path
+    node_colors = ['green' if node in path else 'red' for node in G.nodes()]
+    edge_colors = ['green' if edge in zip(path, path[1:]) else 'black' for edge in G.edges()]
 
-# draw the nodes
-nx.draw_networkx_nodes(G, pos, node_color='w', edgecolors='k', node_size=1000)
+    # Draw initial graph
+    pos = nx.spring_layout(G)
+    nx.draw(G, pos, with_labels=True, font_weight='bold', node_color=node_colors, edge_color=edge_colors)
 
-# draw the labels
-nx.draw_networkx_labels(G, pos, font_size=20, font_family='sans-serif')
+    # Animate shortest path
+    for i in range(len(path)-1):
+        # Highlight current node and edge
+        node_colors = ['green' if node == path[i] else 'red' for node in G.nodes()]
+        edge_colors = ['green' if edge == (path[i], path[i+1]) else 'black' for edge in G.edges()]
 
-# draw the edges
-nx.draw_networkx_edges(G, pos)
+        # Redraw graph
+        nx.draw(G, pos, with_labels=True, font_weight='bold', node_color=node_colors, edge_color=edge_colors)
 
-# show the graph
-plt.axis('off')
-plt.show()
+        # Pause briefly
+        plt.pause(0.5)
+        time.sleep(0.5)
+
+    # Highlight final node
+    node_colors = ['green' if node == path[-1] else 'red' for node in G.nodes()]
+    nx.draw(G, pos, with_labels=True, font_weight='bold', node_color=node_colors, edge_color=edge_colors)
+
+    # Show final graph
+    plt.show()
