9e1058c995eae3aac5b632daf072a9c1c56c8fc0
Mohammad <ismail.mohammad.n@gmail.com>
2023-04-11 21:00:05 -0700

    push2

--- install.bat
+++ install.bat
@@ -3,6 +3,6 @@ call npm i
 cd ../server
 call pip install -r requirements.txt
 cd ../client
-call npm run dev
+start npm run dev
 cd ../server
-start python -m flask --app server run &
+call python -m flask --app server run 
